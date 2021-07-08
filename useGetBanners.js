ae2f38e6d57141d288a31d6ac8012907d451d6ae
yosiaagustadewa <yosiaagustadewa@gmail.com>
2021-07-08 21:15:19 +0700

    up

--- useGetBanners.js
+++ useGetBanners.js
@@ -4,6 +4,6 @@ import useSWR from 'swr'
 export default function useGetBanners() {
   const fetcher = url => fetch(url).then(res => res.json());
 
-  const {data:banners, error} = useSWR('http://localhost:3000/api/banners', fetcher)
+  const {data:banners, error} = useSWR('api/banners', fetcher)
   return {banners, error}
 }
